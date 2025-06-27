e03e8c5a99c4887c91bfa13038378a92431ceb94
Surjendu kar <rahulgotp9988@gmai.com>
2025-06-27 14:53:33 +0530

    Enhance Home component with motion animations: replace static heading with animated motion.h2 for improved visual appeal and user engagement.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,9 +1,21 @@
 import { Link } from 'react-router-dom';
+import { motion } from 'motion/react';
 
 const Home = () => {
   return (
     <div className="home container">
-      <h2>Welcome to Pizza Joint</h2>
+      <motion.h2
+        initial={{ x: '-100vw' }}
+        animate={{
+          fontSize: '70px',
+          x: 0,
+        }}
+        transition={{
+          duration: 4,
+        }}
+      >
+        Welcome to Pizza Joint
+      </motion.h2>
       <Link to="/base">
         <button>Create Your Pizza</button>
       </Link>
