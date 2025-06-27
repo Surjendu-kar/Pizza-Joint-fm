df41442092f02ed1ad930539ad0e6c52127c783a
Surjendu kar <rahulgotp9988@gmai.com>
2025-06-27 12:54:44 +0530

    Update ESLint configuration to include Prettier integration and add relevant dependencies in package.json and package-lock.json. Refactor App component for improved code style and consistency.

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,23 @@
-import { motion } from "motion/react"
+import { motion } from 'motion/react';
 
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <motion.h1 
-      className="text-4xl font-bold capitalize text-white"
-      initial={{ x: "-100vw" }}
-      animate={{ x: 0 }}
-      transition={{
-        duration:2,
-        delay:1,
-        ease:"backInOut"
-      }}
-      > Learning FM</motion.h1>
+      <motion.h1
+        className="text-4xl font-bold capitalize text-white"
+        initial={{ x: '-100vw' }}
+        animate={{ x: 0 }}
+        transition={{
+          duration: 2,
+          delay: 1,
+          ease: 'backInOut',
+        }}
+      >
+        
+        Learning FM
+      </motion.h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
